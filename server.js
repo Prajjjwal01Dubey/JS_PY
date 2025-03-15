496ebf81fe938477c891250c7bc7469f862e9ac6
prajjwal98dubey <prajjwaldubey003@gmail.com>
2025-03-16 00:57:35 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app  = express()
-
-
-app.get("/",(_,res)=>{
-    res.send("<h1>Hello World!!!!</h1>")
-})
-
-
-app.listen(5001,()=>{
-    console.log("Server Listening at 5001!!!")
-})
+const express = require("express");
+const app = express();
+app.get("/", (_, res) => {
+  res.send("<h1>Hello World!!!!</h1>");
+});
+app.get("/home", (_, res) => {
+  res.send("<h1>Welcome Home Page</h1>");
+});
+app.listen(5001, () => {
+  console.log("Server Listening at 5001!!!");
+});
+ 
